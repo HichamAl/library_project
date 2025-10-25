f605597a1f2902c077befacae52702c2f1c8986c
HichamAl <h.almakroudi@icloud.com>
2025-10-25 22:17:09 +0200

    add book object constructor, function to create book using the object constructor

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("test");
+const library = [];
+
+function Book(title, author, numPages, hasRead) {
+    if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.hasRead = hasRead;
+    this.info = function(){
+        return `${title} by ${author}, ${numPages} pages, ${hasRead}`
+    };
+}
+
+function addBookToLibrary(title, author, numPages, hasRead){
+    const newBook = new Book(title, author, numPages, hasRead);
+
+    library.push(newBook);
+}
+
+const book1 = addBookToLibrary('My first book', 'Hicham Almakroudi', 89, 'has read');
