d50eef0716eb844770be2dc0887bdedf395b1a53
HichamAl <h.almakroudi@icloud.com>
2025-10-31 11:28:15 +0100

    add button to change read status

--- main.js
+++ main.js
@@ -1,26 +1,4 @@
-const library = [
-    {
-    uuid: "29b21834-c5c3-466d-9516-32d0d792eb07",
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    numPages: 310,
-    hasRead: "Has read this book"
-  },
-  {
-    uuid: "29b21834-c4d3-466d-9516-32d0d792eb07",
-    title: "1984",
-    author: "George Orwell",
-    numPages: 328,
-    hasRead: "has not yet read this book"
-  },
-  {
-    uuid: "41b21834-c5c3-466d-9516-32d0d792eb07",
-    title: "The Catcher in the Rye",
-    author: "J.D. Salinger",
-    numPages: 277,
-    hasRead: "Has read this book"
-  }
-];
+const library = [];
 
 function Book(uuid, title, author, numPages, hasRead) {
     if (!new.target) {
@@ -34,33 +12,44 @@ function Book(uuid, title, author, numPages, hasRead) {
     this.info = function(){
         return `${title} by ${author}, ${numPages} pages, ${hasRead}`
     };
-}
+};
+
+Book.prototype.changeStatus = function(bookIndex) {
+    if (library[bookIndex].hasRead === 'Has read this book'){
+        library[bookIndex].hasRead = 'has not yet read this book';
+    } else {
+        library[bookIndex].hasRead = 'Has read this book';
+    }
+};
 
 function addBookToLibrary(title, author, numPages, hasRead){
     const uuid = crypto.randomUUID();
     if (hasRead === true){
         hasRead = 'Has read this book';
     } else {
-        hasRead = 'has not yet read this book';
+        hasRead = 'Has not yet read this book';
     }
     const newBook = new Book(uuid, title, author, numPages, hasRead);
     library.push(newBook);
-}
+};
 
 const books = document.getElementById("books");
-library.forEach(getBooks);
-books.classList.add("container");
 function getBooks(book) {
     const bookCard = document.createElement("div");
     const button = document.createElement("button");
+    const statusButton = document.createElement("button");
+    statusButton.textContent = 'Change read status';
+    statusButton.classList.add("readStatus");
+    statusButton.setAttribute("data-id", `${book.uuid}`);
     button.textContent = 'Remove book';
     button.classList.add("removebook");
     button.setAttribute("data-id", `${book.uuid}`);
     bookCard.classList.add("card");
-    bookCard.textContent = `${book.title}, by ${book.author}, Number of Pages: ${book.numPages}, User  ${book.hasRead}`;
+    bookCard.innerHTML = `<p>${book.title}, by ${book.author}, Number of Pages: ${book.numPages}, User  ${book.hasRead}</p>`;
     books.appendChild(bookCard); 
     bookCard.appendChild(button);
-}
+    bookCard.appendChild(statusButton);
+};
 
 const addNewBookForm = document.getElementById("newbook");
 addNewBookForm.addEventListener("click", addNewBook);
@@ -79,15 +68,7 @@ function addNewBook(event){
     document.getElementById("addBookForm").reset();
 
     const found = library.findLast((element) => element);
-    const newBook = document.createElement("div");
-    newBook.classList.add("card");
-    newBook.textContent = `${found.title}, by ${found.author}, Number of Pages: ${found.numPages}, User  ${found.hasRead}`;
-    books.appendChild(newBook);
-    const button = document.createElement("button");
-    button.classList.add("removebook");
-    button.setAttribute("data-id", `${found.uuid}`);
-    button.textContent = 'Remove book';
-    newBook.appendChild(button);
+    getBooks(found);
 
     const removeBookButtons = document.querySelectorAll(".removebook");
     removeBookButtons.forEach((button) => {
@@ -100,18 +81,19 @@ function addNewBook(event){
     });
 });
 
-}
-
-const removeBookButtons = document.querySelectorAll(".removebook");
-removeBookButtons.forEach((button) => {
-    button.addEventListener("click", ()=>{
+const readStatusButtons = document.querySelectorAll(".readStatus");
+readStatusButtons.forEach((button) => {
+    button.addEventListener("click", ()=> {
         let dataId = button.dataset.id;
-        let bookIndexToRemove = library.findIndex(book => book.uuid === dataId);
-        library.splice(bookIndexToRemove, 1)
-        let div = button.parentNode;
-        div.remove();
+        let bookIndex = library.findIndex(book => book.uuid === dataId);
+        library[bookIndex].changeStatus(bookIndex);
+        const bookText = document.querySelector(".card p");
+        bookText.innerHTML = `<p>${library[bookIndex].title}, by ${library[bookIndex].author}, Number of Pages: ${library[bookIndex].numPages}, User  ${library[bookIndex].hasRead}</p>`;
     });
 });
+};
+
+
 
 
 
