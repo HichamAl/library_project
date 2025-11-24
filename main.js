b2f0d2e2294b35a625da27876b78332a7badc652
HichamAl <h.almakroudi@icloud.com>
2025-11-24 20:12:12 +0100

    change book constructor to class

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 const library = [];
 
-function Book(uuid, title, author, numPages, hasRead) {
-    if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
-    }
+class Book {
+    constructor(uuid, title, author, numPages, hasRead) {
     this.uuid = uuid;
     this.title = title;
     this.author = author;
     this.numPages = numPages;
     this.hasRead = hasRead;
-    this.info = function(){
-        return `${title} by ${author}, ${numPages} pages, ${hasRead}`
-    };
 };
 
-Book.prototype.changeStatus = function(bookIndex) {
-    if (library[bookIndex].hasRead === 'Has read this book'){
-        library[bookIndex].hasRead = 'Has not yet read this book';
-    } else {
+    info(){
+        return `${title} by ${author}, ${numPages} pages, ${hasRead}`;
+    }
+
+    changeStatus(bookIndex){
+        if (library[bookIndex].hasRead === 'Has read this book'){
+            library[bookIndex].hasRead = 'Has not yet read this book';
+        } else {
         library[bookIndex].hasRead = 'Has read this book';
     }
-};
+    }
+}
 
 function addBookToLibrary(title, author, numPages, hasRead){
     const uuid = crypto.randomUUID();
